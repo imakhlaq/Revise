5f99097fd7375d3a15dbe6cc24ff21a14e9f3496
imakhlaq <imakhlaqXD@gmail.com>
2024-02-28 23:55:35 +0530

    spread and rest

--- JavaScript/DeepShalow.js
+++ JavaScript/DeepShalow.js
@@ -1,3 +1,4 @@
+/*
 const obj = {
   name: "akhlaq",
   lega: 2,
@@ -36,9 +37,67 @@ const paresed = JSON.parse(person2Deep);
 paresed.rel[1] = "sama";
 paresed.name = "hesham";
 
-console.log(personDeep);
-console.log(paresed);
+//console.log(personDeep);
+//console.log(paresed);
 
-//sturecture clone
-
+//structure clone
 const deepclone2 = structuredClone(personDeep);
+
+//spread and rest
+//...    and ...
+
+
+*/
+
+// rest op
+
+//array destruct
+
+const arr = [1, 2, 3, 4, 4];
+
+const [, , third, fourth] = arr;
+console.log({ third, fourth });
+
+//object destruct
+
+const person = {
+  name: "samad",
+  age: 18,
+  father: "abuuuu",
+  rel: {
+    cousin: "yusuf",
+  },
+};
+
+const {
+  name: nameOfPerson = 10,
+  rel: { cousin },
+} = person;
+
+console.log(cousin);
+
+//spread
+// array,set,map
+
+const arr1 = [1, 2, 3, 4, [10, 4, 5, 3]];
+const arr2 = [...arr1];
+
+//console.log({ arr1, arr2 });
+
+function ran(one, two, three) {
+  //console.log(arguments);
+}
+
+const arr3 = [1, 2, 3, 3, 4, 5, 44, 4436];
+ran(...arr3);
+
+//rest op(aram)
+const [first1, second, ...arr2] = arr;
+console.log({ first1, second, arr2 });
+
+function ran(...arr) {
+  console.log(arr);
+}
+
+const arr3 = [1, 2, 3, 3, 4, 5, 44, 4436];
+ran(1, 2, 3, 4, 3, 5, 5);
